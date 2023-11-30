77a4c8778bfdd31d50ea26a0674f3d1205129ac3
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-12-01 00:08:23 +0300

    Add files via upload

---
+++ HW 8.12.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+class Costomer {//1)кто что 
+public:
+    //2)поля класса(состояние обьекта)
+    double min_budget;
+    double max_budget;
+    bool discount_card;
+    bool club_card;
+    bool cash_or_wire;
+    bool has_package;
+    string name;
+    string surname;
+    string adress;
+    //3)что эта сущность умеет делать-cписок методов
+    void SpendMoney()
+    {
+        cout << "spend some money..." << "\n";
+    }
+    void SelectProduct()
+    {
+        cout << "brainwash the salesman..." << "\n";
+    }
+    void SaySomeThing()
+    {
+        cout << "i dont need a bag" << "\n";
+    }
+};
+
+int main()
+{
+    Costomer costomer;
+    costomer.SaySomeThing();
+}
